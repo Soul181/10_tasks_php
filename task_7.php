7ec0298f64a1b85e93821a81b5c953efbc744a8f
Soul181 <64364995+Soul181@users.noreply.github.com>
2021-10-11 22:33:00 +0300

    Add files via upload

--- task_7.php
+++ task_7.php
@@ -60,29 +60,19 @@
 							// произвожу запрос в БД, по номеру строки которая хранится в marker
 							$sql = "SELECT * FROM `users` WHERE number=$marker"; // формируем команду найти всё в БД
 							$res = mysqli_query($conn, $sql); // отправляем команду в БД
-							$row = mysqli_fetch_row($res);    // получаем ответ из БД
-							// номер элемента в массиве после запроса в БД для примера что содержит
-							// 1  "user_name" => "Roberto R.",
-							// 2  "user_profession" => "Rails Developer",
-							// 3  "user_position"  => "Partner &amp; Contributor",
-							// 4  "access" => "false",
-							// 5  "img_src"=> "img/demo/authors/roberto.png",
-							// 6  "contact"   => "Roberto",
-							// 7  "twitter_name" => "@sildur",
-							// 8  "twitter_link" => "https://twitter.com/@sildur",
-							// 9  "bootstrap_link" => "https://wrapbootstrap.com/user/sildur"
+							$user = mysqli_fetch_array($res);    // получаем ответ из БД
 							?>
-                            <div class="<?php if ($row[4] == "true") {echo "";} else echo "banned ";?>rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
-                                <img src="<?php echo $row[5];?>" alt="<?php echo $row[1];?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
+                            <div class="<?php if ($user["access"] == "true") {echo "";} else echo "banned ";?>rounded-pill bg-white shadow-sm p-2 border-faded mr-3 d-flex flex-row align-items-center justify-content-center flex-shrink-0">
+                                <img src="<?php echo $user["img_src"];?>" alt="<?php echo $user["user_name"];?>" class="img-thumbnail img-responsive rounded-circle" style="width:5rem; height: 5rem;">
                                 <div class="ml-2 mr-3">
                                     <h5 class="m-0">
-                                        <?php echo $row[1]." (".$row[2].")";?>
+                                        <?php echo $user["user_name"]." (".$user["user_profession"].")";?>
                                         <small class="m-0 fw-300">
-                                            <?php echo $row[3];?>
+                                            <?php echo $user["user_position"];?>
                                         </small>
                                     </h5>
-                                    <a href="<?php echo $row[8];?>" class="text-info fs-sm" target="_blank"><?php echo $row[7];?></a> -
-                                    <a href="<?php echo $row[9];?>" class="text-info fs-sm" target="_blank" title="Contact <?php echo $row[6];?>"><i class="fal fa-envelope"></i></a>
+                                    <a href="<?php echo $user["twitter_link"];?>" class="text-info fs-sm" target="_blank"><?php echo $user["twitter_name"];?></a> -
+                                    <a href="<?php echo $user["bootstrap_link"];?>" class="text-info fs-sm" target="_blank" title="Contact <?php echo $user["contact"];?>"><i class="fal fa-envelope"></i></a>
                                 </div>
                             </div>
 							<?php endforeach;?>
